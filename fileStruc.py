f2c41f17f1d8348b52eccc72e71f7ce262156eb3
Christopher-Carey <ccarey40@gmail.com>
2019-10-08 21:15:13 -0500

    make file structure

--- fileStruc.py
+++ fileStruc.py
@@ -14,7 +14,31 @@ def makeStruc():
     os.chdir(path+projectName+"/apps")
     appName=input("App Name")
     os.system("python ../manage.py startapp "+appName) 
-    # ------MAKE APP---------
+    # ------MAKE FILE STRUCTURE---------
+    os.chdir(path+projectName+"/apps/"+appName)
+    os.system("type nul > urls.py")
+    os.system("mkdir templates")
+    os.system("mkdir static")
+    # --templates folder--
+    os.chdir(path+projectName+"/apps/"+appName+"/templates")
+    os.system("mkdir "+appName)
+    os.chdir(path+projectName+"/apps/"+appName+"/templates/"+appName)
+    os.system("type nul > index.html")
+    os.system("cd..")
+    os.system("cd..")
+    # --static folder--
+    os.chdir(path+projectName+"/apps/"+appName+"/static")
+    os.system("mkdir "+appName)
+    os.chdir(path+projectName+"/apps/"+appName+"/static/"+appName)
+    os.system("mkdir css")
+    os.system("mkdir js")
+    os.system("mkdir images")
+    # --static subfolders--
+    os.chdir(path+projectName+"/apps/"+appName+"/static/"+appName+"/css")
+    os.system("type nul > style.css")
+    os.system("cd..")
+    os.chdir(path+projectName+"/apps/"+appName+"/static/"+appName+"/js")
+    os.system("type nul > script.js")
 
 
 
