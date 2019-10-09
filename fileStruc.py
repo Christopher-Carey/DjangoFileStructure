49cb08a813298ab0de1389f4eadb27f940b4b698
Christopher-Carey <ccarey40@gmail.com>
2019-10-08 21:10:43 -0500

    make app folder and initialise app

--- fileStruc.py
+++ fileStruc.py
@@ -1,11 +1,20 @@
 import os
 import time
 
-def makeStruc()
+def makeStruc():
     # ------MAKE PROJECT---------
     path="/Users/ccare/Documents/Coding/Coding Dojo/weekThree/Python/python_stack/django/django_intro/"
     os.chdir(path)
     projectName= input("enter name")
     os.system("django-admin startproject " +projectName)
-    # ------MAKE PROJECT---------
+    # ------MAKE APP---------
+    time.sleep(5)
+    os.chdir(path+projectName)
+    os.system("mkdir apps")
+    os.chdir(path+projectName+"/apps")
+    appName=input("App Name")
+    os.system("python ../manage.py startapp "+appName) 
+    # ------MAKE APP---------
+
+
 
