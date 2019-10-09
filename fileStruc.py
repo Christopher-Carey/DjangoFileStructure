b9ccf1df3cde46cf4db70acaacbf66f2fc41f27e
Christopher-Carey <ccarey40@gmail.com>
2019-10-09 09:06:04 -0500

    added checkboxes(no functionality yet)

--- fileStruc.py
+++ fileStruc.py
@@ -1,12 +1,14 @@
 import os
 import time
 # ----------------GUI-----------------
-from guizero import App, Text, TextBox, PushButton
-app = App(layout="grid")
-pathdir = TextBox(app,width="200",text="enter directory",grid=[0,0])
-projectNamegui = TextBox(app,width="200" ,text="enter project name", align="left",grid=[0,2])
-appNamegui = TextBox(app,width="200",text="enter app name", align="left",grid=[0,4])
-activity = TextBox(app,width="200", align="left",grid=[0,6])
+from guizero import App, Text, TextBox, PushButton,CheckBox
+app = App(layout="grid",width=500)
+pathdir = TextBox(app,width=75,text="enter directory", align="left",grid=[0,0])
+projectNamegui = TextBox(app,width=75,text="enter project name", align="left",grid=[0,2])
+appNamegui = TextBox(app,width=75,text="enter app name", align="left",grid=[0,4])
+windows = CheckBox(app, text="Check for Windows", align="left",grid=[0,9])
+mac = CheckBox(app, text="Check for Mac", align="left",grid=[0,10])
+activity = Text(app,width=75, align="left",text="Activity Here",grid=[0,11])
 # ----------------GUI-----------------
 
 def makeStruc():
@@ -15,10 +17,8 @@ def makeStruc():
     # path="/Users/ccare/Documents/Coding/Coding Dojo/weekThree/Python/python_stack/django/django_intro/"
     os.chdir(path)
     projectName= projectNamegui.value
-    activity.value+=projectName
     # projectName= input("enter name")
     os.system("django-admin startproject " +projectName)
-    activity.value+="Made project"
 
     # ------MAKE APP---------
     time.sleep(5)
@@ -53,11 +53,11 @@ def makeStruc():
     os.system("cd..")
     os.chdir(path+projectName+"/apps/"+appName+"/static/"+appName+"/js")
     os.system("type nul > script.js")
-    os.chdir(path+projectName)
-    os.chdir("code .")
+    # os.chdir(path+projectName)
+    # os.chdir("code .")
 
-
-    print("========ALL DONE===========")
+    activity.value="=========ALL DONE==========="
+    print("=========ALL DONE===========")
 # makeStruc()
 
 # ------------GUI-----------------
