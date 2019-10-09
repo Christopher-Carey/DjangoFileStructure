cb19dbf4d1bf8ef2473354a7344e63cbab499976
Christopher-Carey <ccarey40@gmail.com>
2019-10-08 22:06:15 -0500

    gui inputs working

--- fileStruc.py
+++ fileStruc.py
@@ -11,16 +11,22 @@ activity = TextBox(app,width="200", align="left",grid=[0,6])
 
 def makeStruc():
     # ------MAKE PROJECT---------
-    path="/Users/ccare/Documents/Coding/Coding Dojo/weekThree/Python/python_stack/django/django_intro/"
+    path=pathdir.value
+    # path="/Users/ccare/Documents/Coding/Coding Dojo/weekThree/Python/python_stack/django/django_intro/"
     os.chdir(path)
-    projectName= input("enter name")
+    projectName= projectNamegui.value
+    activity.value+=projectName
+    # projectName= input("enter name")
     os.system("django-admin startproject " +projectName)
+    activity.value+="Made project"
+
     # ------MAKE APP---------
     time.sleep(5)
     os.chdir(path+projectName)
     os.system("mkdir apps")
     os.chdir(path+projectName+"/apps")
-    appName=input("App Name")
+    appName=appNamegui.value
+    # appName=input("App Name")
     os.system("python ../manage.py startapp "+appName) 
     # ------MAKE FILE STRUCTURE---------
     os.chdir(path+projectName+"/apps/"+appName)
